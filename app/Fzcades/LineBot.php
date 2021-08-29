0324e2d9d1b7ce483377d322c55ed134be041538
monica252 <dadada74@gmail.com>
2021-08-29 11:31:25 +0900

    1234z

---
+++ app/Fzcades/LineBot.php
@@ -0,0 +1,5 @@
+<?
+protected static function getFacadeAccessor()
+{
+    return 'line-bot';
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // LINE BOT
+        $this->app->bind("line-bot", function ($app, array $parameters) {
+            // $parametersを見て、SECRETとかTOKENをDBとかNoSQLから取ってくることが多い
+            return new LINEBot(
+                new LINEBot\HTTPClient\CurlHTTPClient(env("LINE_ACCESS_TOKEN")),
+                ["channelSecret" => env("LINE_CHANNEL_SECRET")]
+            );
+        });
+    }
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
